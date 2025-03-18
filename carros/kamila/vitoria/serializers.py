703b4c968e9e734adb73d382c166b0dc9f7d79f7
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-03-18 10:39:19 -0300

    comentei o serializers.py

--- carros/kamila/vitoria/serializers.py
+++ carros/kamila/vitoria/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Carro
-# classe do meu carro
-class CarroSerializer(serializers.ModelSerializer):
+# classe do meu carro - basicamente um convertor de modelo
+class CarroSerializer(serializers.ModelSerializer):#converte json para o meu modelo ou o inverso
     class Meta:
         model = Carro
         fields = '__all__'
